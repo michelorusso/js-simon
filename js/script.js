1bfe262321586f8003137da54829707ef92d8ac9
Michele Lorusso <kirito102095@hotmail.it>
2021-04-08 10:24:08 +0200

    Consegna Esercizio

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@
 
 // Usiamo una funzione che generi 5 numeri random
 
-// arrayNumberRandom -> array vuoto , dove andremo ad inserire 5 numeri (non duplicati) random
+// arrayNumberRandom -> array vuoto dove andremo ad inserire 5 numeri (non duplicati) random
   var arrayNumberRandom = [];
   
     while( arrayNumberRandom.length < 5 ) {
 
+        // fiveNumber -> variabile dove verrà salvato il numero random
         var fiveNumber = fiveRandomNumber(1 , 100);
 
         if( !arrayNumberRandom.includes(fiveNumber) ) {
@@ -24,30 +25,53 @@
     alert(arrayNumberRandom);
     
 // dopo 30 secondi facciamo comparire un prompt dove l'utente inserirà 5 numeri
+
+// arrayNumberUser -> array vuoto dove andremo ad inserire 5 numeri (non duplicati) inseriti dall'utente
 var arrayNumberUser = [];
 
 var userNumber = setTimeout(function() {
 
     while(arrayNumberUser.length < 5) {
 
+        // numberUserInput -> variabile dove verrà salvato il numero inserito dall'utente
         var numberUserInput = parseInt( prompt('Dammi un numero') );
 
-
+        // se il numero da noi inserito non fa parte dell'array ed è inferiore a 100
         if(!arrayNumberUser.includes(numberUserInput) && numberUserInput < 100) {
 
+            // allora lo inserisco nell'array contente i numeri inseriti dall'utente
             arrayNumberUser.push(numberUserInput);
-
+   
         } else {
+            // alrimenti nel caso il numero da noi inserito è uguale ad un numero inserito precedentemente allora compare un alert
             alert('Il numero è gia presente o non è valido, inserire un altro numero');
         }
 
     }
 
-}, 1000);
+    var numberFindIt = [];
+
+    for( var j = 0; j < arrayNumberUser.length; j++) {
+
+        var numbersUser = arrayNumberUser[j];
+        console.log(numbersUser);
+
+        // prendiamo i numeri inseriti dall'utente e controlliamo quanti e quali numeri 
+        // sono uguali a quelli generati random dal computer        
+        if( arrayNumberRandom.includes(numbersUser) == true ) {
+            numberFindIt.push(numbersUser);
+        }
+        console.log('Questo è l array dei numeri indovinati : ', numberFindIt);
+    }
+    
+
+    // Stampiamo i risultati
+    alert("Hai indovinato " + numberFindIt.length + " numeri su 5");
+    alert("I numeri sono : " + numberFindIt);
+
+}, 1000); // TODO : cambiare in 30 secondi
 
 console.log(arrayNumberUser);
-// prendiamo i numeri inseriti dall'utente e controlliamo quanti e quali numeri 
-// sono uguali a quelli generati random dal computer
 
 
 // FUNCTION
